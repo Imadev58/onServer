138901b6af324c1930169c0515337a652f97a116
semlb06 <162473606+semlb06@users.noreply.github.com>
2024-07-09 20:55:43 +0200

    Added footer.php

---
+++ resources/view/footer.php
@@ -0,0 +1,11 @@
+<footer class="bg-white py-8 px-4  z-10 flex justify-content-center align-items-center dark:bg-slate-900 dark:text-white">
+    <div class="text-sm text-gray-600 dark:text-gray-300"><?php echo FOOTER; ?></div>
+    <div class="flex ml-auto gap-4">
+        <a href="terms.php" class="text-gray-700 hover:text-gray-900 dark:text-gray-500 hover:dark:text-gray-700">Terms &
+            Conditions
+        </a>
+        <a href="privacy.php" class="text-gray-700 hover:text-gray-900 dark:text-gray-500 hover:dark:text-gray-700">Privacy
+            Policy
+        </a>
+    </div>
+</footer>
