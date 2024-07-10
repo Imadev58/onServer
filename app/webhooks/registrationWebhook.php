cf5b2819149649085c6ebb163e1058f81f9a661b
semlb06 <162473606+semlb06@users.noreply.github.com>
2024-07-10 17:52:59 +0200

    Create registrationWebhook.php

---
+++ app/webhooks/registrationWebhook.php
@@ -0,0 +1,34 @@
+<?php
+require_once __DIR__ . 'webhookSettings.php';
+
+function sendRegistrationWebhook($firstName) {
+    $webhookUrl = DISCORD_WEBHOOK;
+
+    $data = [
+        'embeds' => [
+            [
+                'title' => "{$firstName} has registered on " . WEBSITE,
+                'color' => hexdec("1a56db"),
+                'footer' => [
+                    'text' => date('Y-m-d H:i:s')
+                ]
+            ]
+        ]
+    ];
+
+    $jsonData = json_encode($data);
+
+    $ch = curl_init($webhookUrl);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    return $response;
+}
+?>
