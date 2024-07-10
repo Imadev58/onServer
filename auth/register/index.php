bc7bf8316d868c7192338df1e6c6104c7b10ba68
semlb06 <162473606+semlb06@users.noreply.github.com>
2024-07-10 12:32:15 +0200

    Added database error message

--- auth/register/index.php
+++ auth/register/index.php
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
     if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+        die("Something went wrong, please contact the developer with the following error: " . $conn->connect_error);
     }
 
     $sql = "INSERT INTO users (FirstName, LastName, Email, Password) VALUES (?, ?, ?, ?)";
