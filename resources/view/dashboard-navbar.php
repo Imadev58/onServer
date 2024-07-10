476338b37702eb93a6ed3b2480040e934942b131
semlb06 <162473606+semlb06@users.noreply.github.com>
2024-07-10 14:12:33 +0200

    Added logout icon to the button

--- resources/view/dashboard-navbar.php
+++ resources/view/dashboard-navbar.php
@@ -6,7 +6,7 @@
     </a>
     <div class="flex md:order-2 space-x-3 rtl:space-x-reverse">
 <a href="<?php echo "http://" . WEBSITE . "/logout"; ?>" class="flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-    Logout
+<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="white" d="M3 21V3h9v2H5v14h7v2zm13-4l-1.375-1.45l2.55-2.55H9v-2h8.175l-2.55-2.55L16 7l5 5z"/></svg> Logout
 </a>
       <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
