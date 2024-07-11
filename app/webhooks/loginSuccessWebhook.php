7b6103707085497a45933dd44c1ea6aa18f48e05
semlb06 <162473606+semlb06@users.noreply.github.com>
2024-07-11 19:10:12 +0200

    Create loginSuccessWebhook.php

---
+++ app/webhooks/loginSuccessWebhook.php
@@ -0,0 +1,35 @@
+<?php
+require_once __DIR__ . '/webhookSettings.php';
+
+function sendLoginSuccessWebhook($firstName, $lastName, $redirectPage) {
+    $webhookUrl = DISCORD_WEBHOOK;
+
+    $data = [
+        'embeds' => [
+            [
+                'title' => "✔️ Login success!",
+                'description' => "> User: `{$firstName} {$lastName}` has logged in successfully and has been redirected to `/{$redirectPage}`.",
+                'color' => hexdec("1a56db"),
+                'footer' => [
+                    'text' => "Event time: " . date('Y-m-d H:i:s')
+                ]
+            ]
+        ]
+    ];
+
+    $jsonData = json_encode($data);
+
+    $ch = curl_init($webhookUrl);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    return $response;
+}
+?>
