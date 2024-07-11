60c0645ddc611febece078a8b724cf0ca0cc0dbc
semlb06 <162473606+semlb06@users.noreply.github.com>
2024-07-11 18:45:06 +0200

    Removed webhook link in the config

--- app/webhooks/webhookSettings.php
+++ app/webhooks/webhookSettings.php
@@ -1,3 +1,3 @@
 <?php
-define('DISCORD_WEBHOOK', 'https://discord.com/api/webhooks/1260623470581252196/bTgXO49I9ZktxMdQXphPhxMCmmOmMYzsVrZNOJiBvXWWCC9ZsEsJ0DtQnjMeY0VM58pZ'); 
+define('DISCORD_WEBHOOK', ''); 
 ?>
