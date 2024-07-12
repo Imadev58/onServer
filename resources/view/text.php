7fefd1c31d8b80f76bb305cb57a593a5d9a3d0ba
semlb06 <162473606+semlb06@users.noreply.github.com>
2024-07-12 20:32:49 +0200

    Settings page for home page title

---
+++ resources/view/text.php
@@ -0,0 +1,13 @@
+<?php
+
+
+define('TITLE', "");
+define('DESCRIPTION', "");
+define('ANNOUNCEMENT_TEXT', "");
+define('ANNOUNCEMENT_SHORT', "");
+define('BUTTONTEXT1', "");
+define('BUTTONTEXT2', "");
+
+
+
+?>
