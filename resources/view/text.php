14a25fbfddedd6a68cb1602918afb833786c25f5
semlb06 <162473606+semlb06@users.noreply.github.com>
2024-07-13 17:06:21 +0200

    Updated the default config (text.php)

--- resources/view/text.php
+++ resources/view/text.php
@@ -1,7 +1,7 @@
 <?php
 
 
-define('TITLE', "Welcome to onServer Billing!");
+define('TITLE', "Welcome to onServer!");
 define('DESCRIPTION', "This is the default page. Everything is set up fine! Next step is to configure all the settings. You can change the title and buttons in <strong>/resources/view/text.php</strong>.");
 define('ANNOUNCEMENT_TEXT', "Thanks for choosing onServer!");
 define('ANNOUNCEMENT_SHORT', "❤️");
