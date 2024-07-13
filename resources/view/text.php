9121806199fed01e0993ba77907f8e3c952c8ca2
semlb06 <162473606+semlb06@users.noreply.github.com>
2024-07-13 17:03:32 +0200

    Added default text, description and buttons to the settings config file

--- resources/view/text.php
+++ resources/view/text.php
@@ -1,12 +1,12 @@
 <?php
 
 
-define('TITLE', "");
-define('DESCRIPTION', "");
-define('ANNOUNCEMENT_TEXT', "");
-define('ANNOUNCEMENT_SHORT', "");
-define('BUTTONTEXT1', "");
-define('BUTTONTEXT2', "");
+define('TITLE', "Welcome to onServer Billing!");
+define('DESCRIPTION', "This is the default page. Everything is set up fine! Next step is to configure all the settings. You can change the title and buttons in <strong>/resources/view/text.php</strong>.");
+define('ANNOUNCEMENT_TEXT', "Thanks for choosing onServer!");
+define('ANNOUNCEMENT_SHORT', "❤️");
+define('BUTTONTEXT1', "Configure settings");
+define('BUTTONTEXT2', "Github");
 
 
 
