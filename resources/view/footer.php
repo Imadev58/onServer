1fdc4b192ffd8230a69f63ae1d9b9e9b079528d3
semlb06 <162473606+semlb06@users.noreply.github.com>
2024-07-14 17:47:10 +0200

    Edited footer and added disclaimer

--- resources/view/footer.php
+++ resources/view/footer.php
@@ -1,11 +1,32 @@
-<footer class="bg-white dark:bg-gray-900 py-6 py-8 px-4  z-10 flex justify-content-center align-items-center dark:bg-slate-900 dark:text-white">
-    <div class="text-sm text-gray-600 dark:text-gray-300"><?php echo FOOTER; ?></div>
+<!--
+
+                                                                                            DISCLAIMER(!)
+                                                        When removing/changing the text and/or link "Powered by onServer" no support is provided.
+
+-->
+
+<footer class="bg-white dark:bg-gray-900 py-6 px-4 z-10 flex justify-between items-center dark:bg-slate-900 dark:text-white relative">
+    <div class="text-sm text-gray-600 dark:text-gray-300">
+        <?php echo FOOTER; ?>
+    </div> 
     <div class="flex ml-auto gap-4">
-        <a href="<?php echo "http://" . WEBSITE . "/terms"; ?>" class="text-gray-700 hover:text-gray-900 dark:text-gray-500 hover:dark:text-gray-700">Terms &
-            Conditions
-        </a>
-        <a href="<?php echo "http://" . WEBSITE . "/privacy"; ?>" class="text-gray-700 hover:text-gray-900 dark:text-gray-500 hover:dark:text-gray-700">Privacy
-            Policy
-        </a>
+        <a href="<?php echo "http://" . WEBSITE . "/terms"; ?>" class="text-gray-700 hover:text-gray-900 dark:text-gray-500 hover:dark:text-gray-700">Terms & Conditions</a>
+        <a href="<?php echo "http://" . WEBSITE . "/privacy"; ?>" class="text-gray-700 hover:text-gray-900 dark:text-gray-500 hover:dark:text-gray-700">Privacy Policy</a>
     </div>
 </footer>
+
+<div class="bg-white dark:bg-gray-900 py-2 px-4 text-center dark:text-white">
+    <span style="display: inline-flex; align-items: center;">
+        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" style="margin-right: 0.5em;">
+            <path fill="none" stroke="#194fcc" stroke-width="2" d="m9 22l6-20m2 15l5-5l-5-5M7 17l-5-5l5-5"/>
+        </svg>
+        <a class="text-white" href="https://github.com/semlb06/onServer" target="_blank">Powered by onServer</a> <!-- Once you remove the text "Powered by onServer" we do not provide any support. -->
+    </span>
+</div>
+
+<!--
+
+                                                                                            DISCLAIMER(!)
+                                                        When removing/changing the text and/or link "Powered by onServer" no support is provided.
+
+-->
